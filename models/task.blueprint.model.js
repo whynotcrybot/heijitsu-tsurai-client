59b263fef576226d8be40fcf5b98992cc37d8970
Cry <alexanderkatsero@gmail.com>
2017-02-25 08:17:14 +0000

    Revert "Merge branch 'feature/types' into 'master'"
    This reverts merge request !4

--- models/task.blueprint.model.js
+++ models/task.blueprint.model.js
@@ -1,35 +1,22 @@
 import mongoose from 'mongoose'
 import q from 'q'
 
-import CompletedTask from '../models/task.completed.model.js'
-
 mongoose.Promise = q.Promise
 
 const blueprintSchema = new mongoose.Schema(
   {
     title           : { type: String },
     active          : { type: Boolean },
-    type            : { type: String, enum: ['continuous', 'singular'] },
+    type            : { type: String },
     createdAt       : { type: Date, default: Date.now },
     updatedAt       : { type: Date, default: Date.now }
-    //DO NOT ADD LAST COMPLETED AT!
   },
   {
     collection: 'task.blueprints'
   }
 )
 blueprintSchema.statics = {
-  findAvailable: function(){
-    this
-      .find()
-      .lean()
-      .distinct('_id')
-      .then(ids => ids.filter( id => {
-        CompletedTask
-          .getByBlueprint(id)
-      }))
-
-
+  findAll: function(){
     return this.find()
   },
   get: function(blueprintID){
@@ -42,12 +29,6 @@ blueprintSchema.statics = {
     return this
       .count({_id : blueprintID})
       .then(count => count ? true : false)
-  },
-  isActive: function(blueprintID){
-    return this
-      .findOne({_id : blueprintID})
-      .select({active: true})
-      .then(task => console.log("task", task))
   }
 }
 
