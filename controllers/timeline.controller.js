ce7cb85c6e77e7357bddbfbdc4bbccd6c74862dd
Alexander Katsero <alexanderkatsero@gmail.com>
2017-03-06 21:26:43 +0000

    week-long timeline

--- controllers/timeline.controller.js
+++ controllers/timeline.controller.js
@@ -5,6 +5,7 @@ export {
 }
 
 function getTimeline(req, res){
+  //todo: understand and rewrite this function
   function getClosestMonday(d = new Date().setHours(0,0,0,0)) {
     d = new Date(d)
     var day = d.getDay(),
@@ -13,34 +14,19 @@ function getTimeline(req, res){
   }
   const monday = getClosestMonday()
 
-  console.log("monday", monday)
-
-  /*const findQuery = [
-      {$unwind: "$completed" },
-      {$match: {"completed.completedAt": {$gt: monday} } },
-      {$group: {_id: "$_id", completed: {$push: "$completed"} } }
-  ];*/
-
   BlueprintTask
     .aggregate([
-      {$unwind: '$completed'},
-      {$match: {"completed.completedAt": { $gt: monday }}}
+      {$unwind : '$completed'},
+      {$match  : {'completed.completedAt': { $gt: monday }}},
+      {$group  : {
+        _id       : '$_id',
+        title     : { $first: '$title' },
+        completed : { $push: '$completed' }
+      }}
     ])
-    .then(x => res.json(x))
-
-    /*
-     * This code snipper finds all tasks that were completed today.
-     * Returns whole object (including complete history of completed tasks)
-     */
-    /*.find({'completed.completedAt' : {$gte : midnight}})
-    .slice('completed', -1)
-    .lean()
-    .then(x => res.json(x))*/
-
-    /*.aggregate(
-      {$unwind: "$completed" },
-      {$match: {"completed.completedAt": {$gt: midnight} } },
-      {$group: {_id: "$_id", completed: {$push: "$completed"} } }
-    )
-    .then(x => res.json(x))*/
+    .then(tasks => res.json(tasks))
+    .catch(error => {
+      console.error("Error:", error)
+      res.json({error})
+    })
 }
