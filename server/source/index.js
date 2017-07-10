9e9bc9735c43662407335cea471a830653422ce7
Alexander Katsero <alexanderkatsero@gmail.com>
2017-07-10 14:13:13 +0300

    constants

--- server/source/index.js
+++ server/source/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 
 import applyMiddlewares from './config/middlewares'
+import constants from './config/constants'
 
 import TaskBlueprint from './routes/task.blueprint.route'
 import Timeline from './routes/timeline.route'
@@ -14,6 +15,19 @@ app.use('/', Timeline)
 
 mongoose.connect('mongodb://mongo/heijitsu-tsurai')
 
-app.listen(3000, () => {
-  console.log('server is ready on 3000')
-})
+// http://www.marcusoft.net/2015/10/eaddrinuse-when-watching-tests-with-mocha-and-supertest.html
+if (!module.parent) {
+  app.listen(constants.PORT, err => {
+    if (err) console.error('Error occured')
+    else {
+      console.log(
+        `
+          Listening on port: ${constants.PORT}
+          Environment: ${process.env.NODE_ENV}
+        `,
+      )
+    }
+  })
+}
+
+export default app
