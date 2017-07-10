3e3ac18b9a688a4ff5fa78aaf9f6305f7eeecaa2
Alexander Katsero <alexanderkatsero@gmail.com>
2017-07-10 12:07:14 +0300

    middlewares

--- server/source/index.js
+++ server/source/index.js
@@ -1,12 +1,14 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
+
+import applyMiddlewares from './config/middlewares'
 
 import TaskBlueprint from './routes/task.blueprint.route'
 import Timeline from './routes/timeline.route'
 
 const app = express()
-app.use(bodyParser.json())
+applyMiddlewares(app)
+
 app.use('/', TaskBlueprint)
 app.use('/', Timeline)
 
