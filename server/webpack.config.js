7c57fd976100d0123ca1d8b1394da44b684bdb76
Alexander Katsero <alexanderkatsero@gmail.com>
2017-07-11 08:31:08 +0300

    uglifyjs -> babili

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const nodeExternals = require('webpack-node-externals')
 
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const BabiliPlugin = require('babili-webpack-plugin')
 const StatsPlugin = require('stats-webpack-plugin')
 
 module.exports = {
@@ -16,23 +16,7 @@ module.exports = {
     libraryTarget: 'commonjs2'
   },
   plugins: [
-    new UglifyJSPlugin({
-      compress: {
-        warnings: false,
-        screw_ie8: true,
-        conditionals: true,
-        unused: true,
-        comparisons: true,
-        sequences: true,
-        dead_code: true,
-        evaluate: true,
-        if_return: true,
-        join_vars: true
-      },
-      output: {
-        comments: false
-      }
-    }),
+    new BabiliPlugin(),
     new StatsPlugin('stats.json', {
       chunkModules: true,
       exclude: /node_modules/
