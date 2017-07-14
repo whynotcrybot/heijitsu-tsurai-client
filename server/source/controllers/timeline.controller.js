bbf9ea09a44cb12c45efd2d3ae000fcea49b526a
Alexander Katsero <alexanderkatsero@gmail.com>
2017-07-14 09:17:38 +0300

    get timeline

--- server/source/controllers/timeline.controller.js
+++ server/source/controllers/timeline.controller.js
@@ -1,35 +1,12 @@
-import moment from 'moment'
+import HTTPStatus from 'http-status'
+
 import BlueprintTask from '../models/task.blueprint.model.js'
 
-export function getTimeline (req, res) {
-  const monday = moment().set({
-    day: 'Monday',
-    hour: 0,
-    minute: 0,
-    second: 0
-  })
-  .toDate()
-
-  BlueprintTask
-    .aggregate([
-      {$unwind: '$completed'},
-      {$lookup: {
-        from: 'completedtasks',
-        localField: 'completed',
-        foreignField: '_id',
-        as: 'completed'
-      }},
-      {$unwind: '$completed'},
-      {$match: {'completed.completedAt': { $gt: monday }}},
-      {$group: {
-        _id: '$_id',
-        title: { $first: '$title' },
-        completed: { $push: '$completed' }
-      }}
-    ])
-    .then(tasks => res.json(tasks))
-    .catch(error => {
-      console.error("Error:", error)
-      res.json({error})
-    })
+export async function getTimeline (req, res, next) {
+  try {
+    return res.json(await BlueprintTask.getTimeline())
+  } catch (e) {
+    e.status = HTTPStatus.BAD_REQUEST
+    return next(e)
+  }
 }
