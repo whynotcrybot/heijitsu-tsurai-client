9bb72a068e5d5551abbd27d35d7d93b8c6800a8e
Alexander Katsero <alexanderkatsero@gmail.com>
2017-07-14 12:29:48 +0300

    initial client commit

---
+++ client/source/js/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Flex, Box } from 'reflexbox'
+
+const Home = () => {
+  return (
+    <Flex justify='center'>
+      <Box w={2 / 3}>
+        <h2>Home</h2>
+      </Box>
+    </Flex>
+  )
+}
+
+export default Home
---
+++ client/source/js/pages/NotFound/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Flex, Box } from 'reflexbox'
+
+const NotFound = () => {
+  return (
+    <Flex justify='center'>
+      <Box w={2 / 3}>
+        <h2>Not Found</h2>
+      </Box>
+    </Flex>
+  )
+}
+
+export default NotFound
