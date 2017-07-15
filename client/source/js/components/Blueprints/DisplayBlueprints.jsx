a592d05f97a03c74a909a055bc41df56f032c687
Alexander Katsero <alexanderkatsero@gmail.com>
2017-07-15 13:29:13 +0300

    a lil' bit of styling

--- client/source/js/components/Blueprints/DisplayBlueprints.jsx
+++ client/source/js/components/Blueprints/DisplayBlueprints.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import * as styles from './styles.css'
+
 const DisplayBlueprints = ({blueprints}) => {
   return (
-    <ul>
-      {blueprints.map(blueprint => <li key={blueprint._id}>{blueprint.title}</li>)}
-    </ul>
+    <div className={styles.blueprintsWrapper}>
+      {
+        blueprints.map(blueprint => {
+          return (
+            <div
+              key={blueprint._id}
+              className={styles.blueprint}
+            >
+              {blueprint.title}
+            </div>
+          )
+        })
+      }
+    </div>
   )
 }
 
