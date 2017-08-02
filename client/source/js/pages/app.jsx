2e55bac4752a4d686c0a90ce86ab22ecbf6fd885
Alexander Katsero <alexanderkatsero@gmail.com>
2017-08-02 09:42:25 +0300

    css loading order fix

--- client/source/js/pages/app.jsx
+++ client/source/js/pages/app.jsx
@@ -3,9 +3,8 @@ import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Flex, Box } from 'reflexbox'
 
-import Routes from './routes'
-
 import '../../style/index.global.css'
+import Routes from './routes'
 
 const Root = (props) => {
   return (
