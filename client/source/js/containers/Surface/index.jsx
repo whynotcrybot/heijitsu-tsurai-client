9868b0aaec1644b6f0ba56f9cafa56795e6d1231
Alexander Katsero <alexanderkatsero@gmail.com>
2017-08-02 09:51:04 +0300

    removed unnecessary parts

--- client/source/js/containers/Surface/index.jsx
+++ client/source/js/containers/Surface/index.jsx
@@ -2,16 +2,11 @@ import React, { Component } from 'react'
 import { Flex, Box } from 'reflexbox'
 
 class Surface extends Component {
-  scrollTo () {
-    this.surface.scrollIntoView()
-    console.log('scrolled')
-  }
-
   render () {
     const { children } = this.props
 
     return (
-      <div ref={ref => (this.surface = ref)}>
+      <div>
         <Flex justify='center'>
           <Box w={2 / 3}>
             {children}
