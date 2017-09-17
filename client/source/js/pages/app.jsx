dd174edce47b8c0a121f203a02e7ac84eb628cfa
whynotcrybot <alexanderkatsero@gmail.com>
2017-09-17 18:56:00 +0300

    index.global.css -> normalize.css

--- client/source/js/pages/app.jsx
+++ client/source/js/pages/app.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { Flex, Box } from 'reflexbox'
 
-import '../../style/index.global.css'
+import '../../style/normalize.css'
 import Routes from './routes'
 
 const Root = (props) => {
   return (
     <Provider store={props.store}>
       <Router>
-        <Flex wrap>
-          <Box w={1}>
-            <Routes />
-          </Box>
-        </Flex>
+        <Routes />
       </Router>
     </Provider>
   )
